5af02dca7038d811a1eb370db9fb151838031528
AlexanderMikhailenko <molotok539@gmail.com>
2022-01-30 20:41:57 +0900

    Add caching for TextContainer

--- MonogameTextEditor/TextEditor/ITextCollection.cs
+++ MonogameTextEditor/TextEditor/ITextCollection.cs
@@ -13,34 +13,44 @@ namespace MonogameTextEditor.TextEditor {
     public class ArrayStringText : ITextCollection {
         public List<string> Text { get; } = new List<string> { "" };
 
+        private string cachedString = "";
+        private bool textUpdated = false;
+
         public void Insert(int line, int col, string text) {
             Text[line] = Text[line].Insert(col, text);
+            textUpdated = true;
         }
 
         public void Remove(int line, int col, int lenght) {
             Text[line] = Text[line].Remove(col, lenght);
+            textUpdated = true;
         }
 
         public void RemoveLine(int line) {
             Text.RemoveAt(line);
+            textUpdated = true;
         }
 
         public void InsertLine(int line, string text) {
             Text.Insert(line, text);
+            textUpdated = true;
         }
 
         public override string ToString() {
-            var builder = new StringBuilder();
-            for (var i = 0; i < Text.Count; i++) {
-                var line = Text[i];
-                var isLastLine = i == Text.Count - 1;
-                if (isLastLine)
-                    builder.Append(line);
-                else
-                    builder.AppendLine(line);
+            if (textUpdated) {
+                var builder = new StringBuilder();
+                for (var i = 0; i < Text.Count; i++) {
+                    var line = Text[i];
+                    var isLastLine = i == Text.Count - 1;
+                    if (isLastLine)
+                        builder.Append(line);
+                    else
+                        builder.AppendLine(line);
+                }
+                cachedString = builder.ToString();
+                textUpdated = false;
             }
-
-            return builder.ToString();
+            return cachedString;
         }
     }
 }
