85c3225434f68994ba1b0a847ed5dd5b577a89ce
AlexanderMikhailenko <molotok539@gmail.com>
2022-02-02 00:43:26 +0900

    Change code style

--- MonogameTextEditor/TextEditor/ISelectEditor.cs
+++ MonogameTextEditor/TextEditor/ISelectEditor.cs
@@ -3,261 +3,260 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MonogameTextEditor.TextEditor {
-    public interface ISelectEditor {
-        ICaretEditor CaretEditor { get; }
-        ICaretPosition StartPosition { get; }
-        ICaretPosition EndPosition { get; }
-        ITextCollection Text { get; }
-        bool MoveSelectRight(int n);
-        bool MoveSelectDown(int n);
-        void Insert(string text);
-        void RemoveSelect();
-        void ClearSelection();
-        bool HasSelection();
-        void RemoveForward();
-        void RemoveBackward();
-        bool MoveCaretRight(int n);
-        bool MoveCaretDown(int n);
-        string GetSelectedText();
-        void MoveSelectToNextWord();
-        void MoveSelectToPrevWord();
-        void MoveCaretToNextWord();
-        void MoveCaretToPrevWord();
-        void SelectAll();
-        void MoveCaretToEndOfLine();
-        void MoveCaretToStartOfLine();
-        string CutSelectedText();
-        void MoveSelectToStartLine();
-        void MoveSelectToEndLine();
-    }
+namespace MonogameTextEditor.TextEditor
+{
+	public interface ISelectEditor
+	{
+		ICaretEditor CaretEditor { get; }
+		ICaretPosition StartPosition { get; }
+		ICaretPosition EndPosition { get; }
+		ITextCollection Text { get; }
+		bool MoveSelectRight(int n);
+		bool MoveSelectDown(int n);
+		void Insert(string text);
+		void RemoveSelect();
+		void ClearSelection();
+		bool HasSelection();
+		void RemoveForward();
+		void RemoveBackward();
+		bool MoveCaretRight(int n);
+		bool MoveCaretDown(int n);
+		string GetSelectedText();
+		void MoveSelectToNextWord();
+		void MoveSelectToPrevWord();
+		void MoveCaretToNextWord();
+		void MoveCaretToPrevWord();
+		void SelectAll();
+		void MoveCaretToEndOfLine();
+		void MoveCaretToStartOfLine();
+		string CutSelectedText();
+		void MoveSelectToStartLine();
+		void MoveSelectToEndLine();
+	}
 
-    public class SelectEditor : ISelectEditor {
-        public ICaretEditor CaretEditor { get; }
-        public ICaretPosition StartPosition { get; } = new DrawableCaret();
-        public ICaretPosition EndPosition { get; } = new DrawableCaret();
-        public ITextCollection Text => CaretEditor.Text;
+	public class SelectEditor : ISelectEditor
+	{
+		public ICaretEditor CaretEditor { get; }
+		public ICaretPosition StartPosition { get; } = new DrawableCaret();
+		public ICaretPosition EndPosition { get; } = new DrawableCaret();
+		public ITextCollection Text => CaretEditor.Text;
 
-        public SelectEditor(ICaretEditor caretEditor) {
-            CaretEditor = caretEditor;
-        }
+		public SelectEditor(ICaretEditor caretEditor)
+		{
+			CaretEditor = caretEditor;
+		}
 
-        public bool MoveSelectRight(int n) {
-            if (!HasSelection())
-                ClearSelection();
+		public bool MoveSelectRight(int n)
+		{
+			if (!HasSelection())
+				ClearSelection();
+			if (CaretEditor.MoveCaretRight(n)) {
+				EndPosition.AssignFrom(CaretEditor.Caret);
+				return true;
+			}
+			return false;
+		}
 
-            if (CaretEditor.MoveCaretRight(n)) {
-                EndPosition.AssignFrom(CaretEditor.Caret);
-                return true;
-            }
+		public bool MoveSelectDown(int n)
+		{
+			if (!HasSelection())
+				ClearSelection();
+			if (CaretEditor.MoveCaretDown(n)) {
+				EndPosition.AssignFrom(CaretEditor.Caret);
+				return true;
+			}
+			return false;
+		}
 
-            return false;
-        }
+		public void Insert(string text)
+		{
+			if (HasSelection())
+				RemoveSelect();
+			CaretEditor.Insert(text);
+		}
 
-        public bool MoveSelectDown(int n) {
-            if (!HasSelection())
-                ClearSelection();
+		public void RemoveSelect()
+		{
+			if (!HasSelection())
+				return;
+			var (firstCaret, secondCaret) = GetSortedCarets();
+			if (firstCaret.Line == secondCaret.Line)
+				Text.Remove(firstCaret.Line, firstCaret.Col, secondCaret.Col - firstCaret.Col);
+			else {
+				Text.Remove(firstCaret.Line, firstCaret.Col,
+					Text[firstCaret.Line].Length - firstCaret.Col);
+				for (var i = firstCaret.Line + 1; i < secondCaret.Line; i++)
+					Text.RemoveLine(firstCaret.Line + 1);
+				Text.Remove(firstCaret.Line + 1, 0, secondCaret.Col);
+				Text.Insert(firstCaret.Line, firstCaret.Col, Text[firstCaret.Line + 1]);
+				Text.RemoveLine(firstCaret.Line + 1);
+			}
+			CaretEditor.Caret.AssignFrom(firstCaret);
+			ClearSelection();
+		}
 
-            if (CaretEditor.MoveCaretDown(n)) {
-                EndPosition.AssignFrom(CaretEditor.Caret);
-                return true;
-            }
+		public void ClearSelection()
+		{
+			StartPosition.AssignFrom(CaretEditor.Caret);
+			EndPosition.AssignFrom(CaretEditor.Caret);
+		}
 
-            return false;
-        }
+		public bool HasSelection()
+		{
+			return !StartPosition.Equals(EndPosition);
+		}
 
-        public void Insert(string text) {
-            if (HasSelection())
-                RemoveSelect();
+		public void RemoveForward()
+		{
+			if (HasSelection())
+				RemoveSelect();
+			else
+				CaretEditor.RemoveForward();
+		}
 
-            CaretEditor.Insert(text);
-        }
+		public void RemoveBackward()
+		{
+			if (HasSelection())
+				RemoveSelect();
+			else
+				CaretEditor.RemoveBackward();
+		}
 
-        public void RemoveSelect() {
-            if (!HasSelection())
-                return;
+		public bool MoveCaretRight(int n)
+		{
+			if (!HasSelection())
+				return CaretEditor.MoveCaretRight(n);
+			var (firstCaret, secondCaret) = GetSortedCarets();
+			CaretEditor.Caret.AssignFrom(n < 0 ? firstCaret : secondCaret);
+			ClearSelection();
+			return true;
+		}
 
-            var (firstCaret, secondCaret) = GetSortedCarets();
+		public bool MoveCaretDown(int n)
+		{
+			if (HasSelection()) {
+				var (firstCaret, secondCaret) = GetSortedCarets();
+				CaretEditor.Caret.AssignFrom(n < 0 ? firstCaret : secondCaret);
+				ClearSelection();
+			}
+			return CaretEditor.MoveCaretDown(n);
+		}
 
-            if (firstCaret.Line == secondCaret.Line) {
-                Text.Remove(firstCaret.Line, firstCaret.Col, secondCaret.Col - firstCaret.Col);
-            } else {
-                Text.Remove(firstCaret.Line, firstCaret.Col,
-                    Text[firstCaret.Line].Length - firstCaret.Col);
+		public string GetSelectedText()
+		{
+			if (!HasSelection())
+				return CaretEditor.GetCurrentLine();
+			var res = new StringBuilder();
+			var (firstCaret, secondCaret) = GetSortedCarets();
+			if (firstCaret.Line == secondCaret.Line)
+				res.Append(Text[firstCaret.Line][firstCaret.Col..secondCaret.Col]);
+			else {
+				res.AppendLine(Text[firstCaret.Line][firstCaret.Col..Text[firstCaret.Line].Length]);
+				for (var i = firstCaret.Line + 1; i < secondCaret.Line; i++)
+					res.AppendLine(Text[i]);
+				res.Append(Text[secondCaret.Line][0..secondCaret.Col]);
+			}
+			return res.ToString();
+		}
 
-                for (var i = firstCaret.Line + 1; i < secondCaret.Line; i++) {
-                    Text.RemoveLine(firstCaret.Line + 1);
-                }
+		public void MoveSelectToNextWord()
+		{
+			if (!HasSelection())
+				ClearSelection();
+			CaretEditor.MoveCaretToNextWord();
+			EndPosition.AssignFrom(CaretEditor.Caret);
+		}
 
-                Text.Remove(firstCaret.Line + 1, 0, secondCaret.Col);
-                Text.Insert(firstCaret.Line, firstCaret.Col, Text[firstCaret.Line + 1]);
-                Text.RemoveLine(firstCaret.Line + 1);
-            }
+		public void MoveSelectToPrevWord()
+		{
+			if (!HasSelection())
+				ClearSelection();
+			CaretEditor.MoveCaretToPrevWord();
+			EndPosition.AssignFrom(CaretEditor.Caret);
+		}
 
-            CaretEditor.Caret.AssignFrom(firstCaret);
-            ClearSelection();
-        }
+		public void MoveCaretToNextWord()
+		{
+			if (HasSelection())
+				ClearSelection();
+			CaretEditor.MoveCaretToNextWord();
+		}
 
-        public void ClearSelection() {
-            StartPosition.AssignFrom(CaretEditor.Caret);
-            EndPosition.AssignFrom(CaretEditor.Caret);
-        }
+		public void MoveCaretToPrevWord()
+		{
+			if (HasSelection())
+				ClearSelection();
+			CaretEditor.MoveCaretToPrevWord();
+		}
 
-        public bool HasSelection() => !StartPosition.Equals(EndPosition);
+		public void SelectAll()
+		{
+			StartPosition.Line = 0;
+			StartPosition.Col = 0;
+			EndPosition.Line = Text.GetLineCount() - 1;
+			EndPosition.Col = Text[EndPosition.Line].Length;
+			CaretEditor.Caret.AssignFrom(EndPosition);
+		}
 
-        public void RemoveForward() {
-            if (HasSelection())
-                RemoveSelect();
-            else
-                CaretEditor.RemoveForward();
-        }
+		public void MoveCaretToEndOfLine()
+		{
+			if (HasSelection()) {
+				var (_, secondCaret) = GetSortedCarets();
+				CaretEditor.Caret.AssignFrom(secondCaret);
+				ClearSelection();
+			}
+			CaretEditor.MoveCaretToEndOfLine();
+		}
 
-        public void RemoveBackward() {
-            if (HasSelection())
-                RemoveSelect();
-            else
-                CaretEditor.RemoveBackward();
-        }
+		public void MoveCaretToStartOfLine()
+		{
+			if (HasSelection()) {
+				var (firstCaret, _) = GetSortedCarets();
+				CaretEditor.Caret.AssignFrom(firstCaret);
+				ClearSelection();
+			}
+			CaretEditor.MoveCaretToStartOfLine();
+		}
 
-        public bool MoveCaretRight(int n) {
-            if (!HasSelection())
-                return CaretEditor.MoveCaretRight(n);
-            var (firstCaret, secondCaret) = GetSortedCarets();
-            CaretEditor.Caret.AssignFrom(n < 0 ? firstCaret : secondCaret);
-            ClearSelection();
-            return true;
-        }
+		public string CutSelectedText()
+		{
+			var res = GetSelectedText();
+			if (HasSelection())
+				RemoveSelect();
+			else {
+				CaretEditor.MoveCaretToStartOfLine();
+				var pos = CaretEditor.Caret;
+				if (Text.GetLineCount() == pos.Line + 1)
+					Text.Remove(pos.Line, 0, Text[pos.Line].Length);
+				else
+					Text.RemoveLine(pos.Line);
+			}
+			return res;
+		}
 
-        public bool MoveCaretDown(int n) {
-            if (HasSelection()) {
-                var (firstCaret, secondCaret) = GetSortedCarets();
-                CaretEditor.Caret.AssignFrom(n < 0 ? firstCaret : secondCaret);
-                ClearSelection();
-            }
+		public void MoveSelectToStartLine()
+		{
+			if (!HasSelection())
+				StartPosition.AssignFrom(CaretEditor.Caret);
+			CaretEditor.MoveCaretToStartOfLine();
+			EndPosition.AssignFrom(CaretEditor.Caret);
+		}
 
-            return CaretEditor.MoveCaretDown(n);
-        }
+		public void MoveSelectToEndLine()
+		{
+			if (!HasSelection())
+				StartPosition.AssignFrom(CaretEditor.Caret);
+			CaretEditor.MoveCaretToEndOfLine();
+			EndPosition.AssignFrom(CaretEditor.Caret);
+		}
 
-        public string GetSelectedText() {
-            if (!HasSelection())
-                return CaretEditor.GetCurrentLine();
-
-            var res = new StringBuilder();
-            var (firstCaret, secondCaret) = GetSortedCarets();
-
-            if (firstCaret.Line == secondCaret.Line) {
-                res.Append(Text[firstCaret.Line][firstCaret.Col..secondCaret.Col]);
-            } else {
-                res.AppendLine(Text[firstCaret.Line][firstCaret.Col..Text[firstCaret.Line].Length]);
-
-                for (var i = firstCaret.Line + 1; i < secondCaret.Line; i++) {
-                    res.AppendLine(Text[i]);
-                }
-
-                res.Append(Text[secondCaret.Line][0..secondCaret.Col]);
-            }
-
-            return res.ToString();
-        }
-
-        public void MoveSelectToNextWord() {
-            if (!HasSelection())
-                ClearSelection();
-
-            CaretEditor.MoveCaretToNextWord();
-            EndPosition.AssignFrom(CaretEditor.Caret);
-        }
-
-        public void MoveSelectToPrevWord() {
-            if (!HasSelection())
-                ClearSelection();
-
-            CaretEditor.MoveCaretToPrevWord();
-            EndPosition.AssignFrom(CaretEditor.Caret);
-        }
-
-        public void MoveCaretToNextWord() {
-            if (HasSelection())
-                ClearSelection();
-
-            CaretEditor.MoveCaretToNextWord();
-        }
-
-        public void MoveCaretToPrevWord() {
-            if (HasSelection())
-                ClearSelection();
-
-            CaretEditor.MoveCaretToPrevWord();
-        }
-
-        public void SelectAll() {
-            StartPosition.Line = 0;
-            StartPosition.Col = 0;
-            EndPosition.Line = Text.GetLineCount() - 1;
-            EndPosition.Col = Text[EndPosition.Line].Length;
-            CaretEditor.Caret.AssignFrom(EndPosition);
-        }
-
-        public void MoveCaretToEndOfLine() {
-            if (HasSelection()) {
-                var (_, secondCaret) = GetSortedCarets();
-                CaretEditor.Caret.AssignFrom(secondCaret);
-                ClearSelection();
-            }
-
-            CaretEditor.MoveCaretToEndOfLine();
-        }
-
-        public void MoveCaretToStartOfLine() {
-            if (HasSelection()) {
-                var (firstCaret, _) = GetSortedCarets();
-                CaretEditor.Caret.AssignFrom(firstCaret);
-                ClearSelection();
-            }
-
-            CaretEditor.MoveCaretToStartOfLine();
-        }
-
-        public string CutSelectedText() {
-            var res = GetSelectedText();
-            if (HasSelection()) {
-                RemoveSelect();
-            }
-            else {
-                CaretEditor.MoveCaretToStartOfLine();
-                var pos = CaretEditor.Caret;
-                if (Text.GetLineCount() == pos.Line + 1) {
-                    Text.Remove(pos.Line, 0, Text[pos.Line].Length);
-                }
-                else {
-                    Text.RemoveLine(pos.Line);
-                }
-            }
-            return res;
-        }
-
-        public void MoveSelectToStartLine() {
-            if (!HasSelection())
-                StartPosition.AssignFrom(CaretEditor.Caret);
-            CaretEditor.MoveCaretToStartOfLine();
-            EndPosition.AssignFrom(CaretEditor.Caret);
-        }
-
-        public void MoveSelectToEndLine() {
-            if (!HasSelection())
-                StartPosition.AssignFrom(CaretEditor.Caret);
-            CaretEditor.MoveCaretToEndOfLine();
-            EndPosition.AssignFrom(CaretEditor.Caret);
-        }
-
-        private (ICaretPosition firstCaret, ICaretPosition secondCaret) GetSortedCarets() {
-            var firstCaret = StartPosition;
-            var secondCaret = EndPosition;
-
-            if (firstCaret > secondCaret)
-                (firstCaret, secondCaret) = (secondCaret, firstCaret);
-
-            return (firstCaret, secondCaret);
-        }
-    }
+		private (ICaretPosition firstCaret, ICaretPosition secondCaret) GetSortedCarets()
+		{
+			var firstCaret = StartPosition;
+			var secondCaret = EndPosition;
+			if (firstCaret > secondCaret)
+				(firstCaret, secondCaret) = (secondCaret, firstCaret);
+			return (firstCaret, secondCaret);
+		}
+	}
 }
--- MonogameTextEditor/TextEditor/ITextCollection.cs
+++ MonogameTextEditor/TextEditor/ITextCollection.cs
@@ -1,64 +1,81 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MonogameTextEditor.TextEditor {
-    public interface ITextCollection {
-        void Insert(int line, int col, string text);
-        void Remove(int line, int col, int lenght);
-        void InsertLine(int line, string text);
-        void RemoveLine(int line);
-        string GetLine(int line);
-        int GetLineLenght(int line);
-        int GetLineCount();
-        string this[int line] => GetLine(line);
-    }
+namespace MonogameTextEditor.TextEditor
+{
+	public interface ITextCollection
+	{
+		void Insert(int line, int col, string text);
+		void Remove(int line, int col, int lenght);
+		void InsertLine(int line, string text);
+		void RemoveLine(int line);
+		string GetLine(int line);
+		int GetLineLenght(int line);
+		int GetLineCount();
+		string this[int line] => GetLine(line);
+	}
 
-    public class ArrayStringText : ITextCollection {
-        private readonly List<string> text = new List<string> { "" };
-        private string cachedString = "";
-        private bool textUpdated = false;
+	public class ArrayStringText : ITextCollection
+	{
+		private readonly List<string> text = new List<string> { "" };
+		private string cachedString = "";
+		private bool textUpdated;
 
-        public void Insert(int line, int col, string text) {
-            this.text[line] = this.text[line].Insert(col, text);
-            textUpdated = true;
-        }
+		public void Insert(int line, int col, string text)
+		{
+			this.text[line] = this.text[line].Insert(col, text);
+			textUpdated = true;
+		}
 
-        public void Remove(int line, int col, int lenght) {
-            text[line] = text[line].Remove(col, lenght);
-            textUpdated = true;
-        }
+		public void Remove(int line, int col, int lenght)
+		{
+			text[line] = text[line].Remove(col, lenght);
+			textUpdated = true;
+		}
 
-        public void RemoveLine(int line) {
-            text.RemoveAt(line);
-            textUpdated = true;
-        }
+		public void RemoveLine(int line)
+		{
+			text.RemoveAt(line);
+			textUpdated = true;
+		}
 
-        public string GetLine(int line) => text[line];
+		public string GetLine(int line)
+		{
+			return text[line];
+		}
 
-        public int GetLineLenght(int line) => text[line].Length;
+		public int GetLineLenght(int line)
+		{
+			return text[line].Length;
+		}
 
-        public int GetLineCount() => text.Count;
+		public int GetLineCount()
+		{
+			return text.Count;
+		}
 
-        public void InsertLine(int line, string text) {
-            this.text.Insert(line, text);
-            textUpdated = true;
-        }
+		public void InsertLine(int line, string text)
+		{
+			this.text.Insert(line, text);
+			textUpdated = true;
+		}
 
-        public override string ToString() {
-            if (textUpdated) {
-                var builder = new StringBuilder();
-                for (var i = 0; i < text.Count; i++) {
-                    var line = text[i];
-                    var isLastLine = i == text.Count - 1;
-                    if (isLastLine)
-                        builder.Append(line);
-                    else
-                        builder.AppendLine(line);
-                }
-                cachedString = builder.ToString();
-                textUpdated = false;
-            }
-            return cachedString;
-        }
-    }
+		public override string ToString()
+		{
+			if (textUpdated) {
+				var builder = new StringBuilder();
+				for (var i = 0; i < text.Count; i++) {
+					var line = text[i];
+					var isLastLine = i == text.Count - 1;
+					if (isLastLine)
+						builder.Append(line);
+					else
+						builder.AppendLine(line);
+				}
+				cachedString = builder.ToString();
+				textUpdated = false;
+			}
+			return cachedString;
+		}
+	}
 }
