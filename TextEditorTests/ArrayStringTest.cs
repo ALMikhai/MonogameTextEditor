290ec7ce8cf12ac341a44b6ae8b58d8332582c10
AlexanderMikhailenko <molotok539@gmail.com>
2022-02-02 20:09:16 +0900

    Change code style for tests

--- TextEditorTests/ArrayStringTest.cs
+++ TextEditorTests/ArrayStringTest.cs
@@ -1,46 +1,53 @@
 using MonogameTextEditor.TextEditor;
 using NUnit.Framework;
 
-namespace TextEditorTests {
-    [TestFixture]
-    public class ArrayStringTest {
-        [Test]
-        public void Create() {
-            var text = new ArrayStringText();
-            Assert.AreEqual(text.ToString(), "");
-        }
+namespace TextEditorTests
+{
+	[TestFixture]
+	public class ArrayStringTest
+	{
+		[Test]
+		public void Create()
+		{
+			var text = new ArrayStringText();
+			Assert.AreEqual(text.ToString(), "");
+		}
 
-        [Test]
-        public void InsertLine() {
-            var text = new ArrayStringText();
-            text.Insert(0, 0, "Hello world!");
-            Assert.AreEqual(text.ToString(), "Hello world!");
-        }
+		[Test]
+		public void InsertLine()
+		{
+			var text = new ArrayStringText();
+			text.Insert(0, 0, "Hello world!");
+			Assert.AreEqual(text.ToString(), "Hello world!");
+		}
 
-        [Test]
-        public void InsertTwoLine() {
-            var text = new ArrayStringText();
-            text.Insert(0, 0, "Hello world!");
-            text.InsertLine(1, "123");
-            Assert.AreEqual(text.ToString(), "Hello world!\r\n123");
-        }
+		[Test]
+		public void InsertTwoLine()
+		{
+			var text = new ArrayStringText();
+			text.Insert(0, 0, "Hello world!");
+			text.InsertLine(1, "123");
+			Assert.AreEqual(text.ToString(), "Hello world!\r\n123");
+		}
 
-        [Test]
-        public void RemoveSymbol() {
-            var text = new ArrayStringText();
-            text.Insert(0, 0, "Hello world!");
-            text.Remove(0, 2, 3);
-            Assert.AreEqual(text.ToString(), "He world!");
-        }
+		[Test]
+		public void RemoveSymbol()
+		{
+			var text = new ArrayStringText();
+			text.Insert(0, 0, "Hello world!");
+			text.Remove(0, 2, 3);
+			Assert.AreEqual(text.ToString(), "He world!");
+		}
 
-        [Test]
-        public void RemoveLine() {
-            var text = new ArrayStringText();
-            text.Insert(0, 0, "Hello world!");
-            text.InsertLine(1, "123");
-            text.InsertLine(1, "321");
-            text.RemoveLine(0);
-            Assert.AreEqual(text.ToString(), "321\r\n123");
-        }
-    }
+		[Test]
+		public void RemoveLine()
+		{
+			var text = new ArrayStringText();
+			text.Insert(0, 0, "Hello world!");
+			text.InsertLine(1, "123");
+			text.InsertLine(1, "321");
+			text.RemoveLine(0);
+			Assert.AreEqual(text.ToString(), "321\r\n123");
+		}
+	}
 }
--- TextEditorTests/CaretTextEditorTest.cs
+++ TextEditorTests/CaretTextEditorTest.cs
@@ -3,196 +3,212 @@ using System.Text;
 using MonogameTextEditor.TextEditor;
 using NUnit.Framework;
 
-namespace TextEditorTests {
-    [TestFixture]
-    public class CaretTextEditorTest {
-        [Test]
-        public void Create() {
-            var editor = new CaretEditor();
-            Assert.AreEqual(editor.Text.ToString(), "");
-            Assert.AreEqual(editor.Caret.Line, 0);
-            Assert.AreEqual(editor.Caret.Col, 0);
-        }
+namespace TextEditorTests
+{
+	[TestFixture]
+	public class CaretTextEditorTest
+	{
+		[Test]
+		public void Create()
+		{
+			var editor = new CaretEditor();
+			Assert.AreEqual(editor.Text.ToString(), "");
+			Assert.AreEqual(editor.Caret.Line, 0);
+			Assert.AreEqual(editor.Caret.Col, 0);
+		}
 
-        [Test]
-        public void InsertLine() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!");
-            Assert.AreEqual(editor.Caret.Line, 0);
-            Assert.AreEqual(editor.Caret.Col, 12);
-        }
+		[Test]
+		public void InsertLine()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!");
+			Assert.AreEqual(editor.Caret.Line, 0);
+			Assert.AreEqual(editor.Caret.Col, 12);
+		}
 
-        [Test]
-        public void InsertTwoLine() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.Insert("\n");
-            editor.Insert("123");
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123");
-            Assert.AreEqual(editor.Caret.Line, 1);
-            Assert.AreEqual(editor.Caret.Col, 3);
-        }
+		[Test]
+		public void InsertTwoLine()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.Insert("\n");
+			editor.Insert("123");
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123");
+			Assert.AreEqual(editor.Caret.Line, 1);
+			Assert.AreEqual(editor.Caret.Col, 3);
+		}
 
-        [Test]
-        public void MoveCaretLeft() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.MoveCaretRight(-2);
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!");
-            Assert.AreEqual(editor.Caret.Line, 0);
-            Assert.AreEqual(editor.Caret.Col, 10);
-        }
+		[Test]
+		public void MoveCaretLeft()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.MoveCaretRight(-2);
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!");
+			Assert.AreEqual(editor.Caret.Line, 0);
+			Assert.AreEqual(editor.Caret.Col, 10);
+		}
 
-        [Test]
-        public void MoveCaretRight() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.MoveCaretRight(-5);
-            editor.MoveCaretRight(4);
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!");
-            Assert.AreEqual(editor.Caret.Line, 0);
-            Assert.AreEqual(editor.Caret.Col, 11);
-        }
+		[Test]
+		public void MoveCaretRight()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.MoveCaretRight(-5);
+			editor.MoveCaretRight(4);
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!");
+			Assert.AreEqual(editor.Caret.Line, 0);
+			Assert.AreEqual(editor.Caret.Col, 11);
+		}
 
-        [Test]
-        public void MoveCaretUp() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.Insert("\n");
-            editor.Insert("123");
-            editor.Insert("\n");
-            editor.Insert("321");
-            editor.MoveCaretDown(-2);
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
-            Assert.AreEqual(editor.Caret.Line, 0);
-            Assert.AreEqual(editor.Caret.Col, 3);
-        }
+		[Test]
+		public void MoveCaretUp()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.Insert("\n");
+			editor.Insert("123");
+			editor.Insert("\n");
+			editor.Insert("321");
+			editor.MoveCaretDown(-2);
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
+			Assert.AreEqual(editor.Caret.Line, 0);
+			Assert.AreEqual(editor.Caret.Col, 3);
+		}
 
-        [Test]
-        public void MoveCaretDown() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.Insert("\n");
-            editor.Insert("123");
-            editor.Insert("\n");
-            editor.Insert("321");
-            editor.MoveCaretDown(-2);
-            editor.MoveCaretDown(1);
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
-            Assert.AreEqual(editor.Caret.Line, 1);
-            Assert.AreEqual(editor.Caret.Col, 3);
-        }
+		[Test]
+		public void MoveCaretDown()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.Insert("\n");
+			editor.Insert("123");
+			editor.Insert("\n");
+			editor.Insert("321");
+			editor.MoveCaretDown(-2);
+			editor.MoveCaretDown(1);
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
+			Assert.AreEqual(editor.Caret.Line, 1);
+			Assert.AreEqual(editor.Caret.Col, 3);
+		}
 
-        [Test]
-        public void MoveCaretRightAtEndOfLine() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.Insert("\n");
-            editor.Insert("123");
-            editor.Insert("\n");
-            editor.Insert("321");
-            editor.MoveCaretDown(-1);
-            editor.MoveCaretRight(1);
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
-            Assert.AreEqual(editor.Caret.Line, 2);
-            Assert.AreEqual(editor.Caret.Col, 0);
-        }
+		[Test]
+		public void MoveCaretRightAtEndOfLine()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.Insert("\n");
+			editor.Insert("123");
+			editor.Insert("\n");
+			editor.Insert("321");
+			editor.MoveCaretDown(-1);
+			editor.MoveCaretRight(1);
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
+			Assert.AreEqual(editor.Caret.Line, 2);
+			Assert.AreEqual(editor.Caret.Col, 0);
+		}
 
-        [Test]
-        public void MoveCaretLeftAtStartOfLine() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.Insert("\n");
-            editor.Insert("123");
-            editor.Insert("\n");
-            editor.Insert("321");
-            editor.MoveCaretRight(-4);
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
-            Assert.AreEqual(editor.Caret.Line, 1);
-            Assert.AreEqual(editor.Caret.Col, 3);
-        }
+		[Test]
+		public void MoveCaretLeftAtStartOfLine()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.Insert("\n");
+			editor.Insert("123");
+			editor.Insert("\n");
+			editor.Insert("321");
+			editor.MoveCaretRight(-4);
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123\r\n321");
+			Assert.AreEqual(editor.Caret.Line, 1);
+			Assert.AreEqual(editor.Caret.Col, 3);
+		}
 
-        [Test]
-        public void RemoveBackward() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.RemoveBackward();
-            Assert.AreEqual(editor.Text.ToString(), "Hello world");
-            Assert.AreEqual(editor.Caret.Line, 0);
-            Assert.AreEqual(editor.Caret.Col, 11);
-        }
+		[Test]
+		public void RemoveBackward()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.RemoveBackward();
+			Assert.AreEqual(editor.Text.ToString(), "Hello world");
+			Assert.AreEqual(editor.Caret.Line, 0);
+			Assert.AreEqual(editor.Caret.Col, 11);
+		}
 
-        [Test]
-        public void RemoveForward() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.MoveCaretRight(-3);
-            editor.RemoveForward();
-            Assert.AreEqual(editor.Text.ToString(), "Hello word!");
-            Assert.AreEqual(editor.Caret.Line, 0);
-            Assert.AreEqual(editor.Caret.Col, 9);
-        }
+		[Test]
+		public void RemoveForward()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.MoveCaretRight(-3);
+			editor.RemoveForward();
+			Assert.AreEqual(editor.Text.ToString(), "Hello word!");
+			Assert.AreEqual(editor.Caret.Line, 0);
+			Assert.AreEqual(editor.Caret.Col, 9);
+		}
 
-        [Test]
-        public void RemoveBackwardAtStartOfLine() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.Insert("\n");
-            editor.Insert("123");
-            editor.Insert("\n");
-            editor.Insert("321");
-            editor.MoveCaretRight(-3);
-            editor.RemoveBackward();
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123321");
-            Assert.AreEqual(editor.Caret.Line, 1);
-            Assert.AreEqual(editor.Caret.Col, 3);
-        }
+		[Test]
+		public void RemoveBackwardAtStartOfLine()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.Insert("\n");
+			editor.Insert("123");
+			editor.Insert("\n");
+			editor.Insert("321");
+			editor.MoveCaretRight(-3);
+			editor.RemoveBackward();
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123321");
+			Assert.AreEqual(editor.Caret.Line, 1);
+			Assert.AreEqual(editor.Caret.Col, 3);
+		}
 
-        [Test]
-        public void RemoveForwardAtEndOfLine() {
-            var editor = new CaretEditor();
-            editor.Insert("Hello world!");
-            editor.Insert("\n");
-            editor.Insert("123");
-            editor.Insert("\n");
-            editor.Insert("321");
-            editor.MoveCaretDown(-1);
-            editor.RemoveForward();
-            Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123321");
-            Assert.AreEqual(editor.Caret.Line, 1);
-            Assert.AreEqual(editor.Caret.Col, 3);
-        }
+		[Test]
+		public void RemoveForwardAtEndOfLine()
+		{
+			var editor = new CaretEditor();
+			editor.Insert("Hello world!");
+			editor.Insert("\n");
+			editor.Insert("123");
+			editor.Insert("\n");
+			editor.Insert("321");
+			editor.MoveCaretDown(-1);
+			editor.RemoveForward();
+			Assert.AreEqual(editor.Text.ToString(), "Hello world!\r\n123321");
+			Assert.AreEqual(editor.Caret.Line, 1);
+			Assert.AreEqual(editor.Caret.Col, 3);
+		}
 
-        [Test]
-        public void Stress() {
-            var editor = new CaretEditor();
-            var n = 10000000;
-            var rand = new Random();
+		[Test]
+		public void Stress()
+		{
+			var editor = new CaretEditor();
+			var n = 10000000;
+			var rand = new Random();
 
-            for (var i = 0; i < n; i++) {
-                var action = rand.Next() % 8;
-                switch (action) {
-                    case 0:
-                        editor.Insert(Utils.BuildRandomString(rand.Next() % 10));
-                        break;
-                    case 1:
-                        editor.Insert("\n");
-                        break;
-                    case 2:
-                        for (var j = 0; j < 5; j++)
-                            editor.RemoveBackward();
-                        break;
-                    case 3:
-                        for (var j = 0; j < 5; j++)
-                            editor.RemoveForward();
-                        break;
-                    default:
-                        editor.MoveCaretRight(rand.Next() % 7 * (rand.Next() % 2 == 1 ? -1 : 1));
-                        editor.MoveCaretDown(rand.Next() % 7 * (rand.Next() % 2 == 1 ? -1 : 1));
-                        break;
-                }
-            }
-        }
-    }
+			for (var i = 0; i < n; i++) {
+				var action = rand.Next() % 8;
+				switch (action) {
+					case 0:
+						editor.Insert(Utils.BuildRandomString(rand.Next() % 10));
+						break;
+					case 1:
+						editor.Insert("\n");
+						break;
+					case 2:
+						for (var j = 0; j < 5; j++)
+							editor.RemoveBackward();
+						break;
+					case 3:
+						for (var j = 0; j < 5; j++)
+							editor.RemoveForward();
+						break;
+					default:
+						editor.MoveCaretRight(rand.Next() % 7 * (rand.Next() % 2 == 1 ? -1 : 1));
+						editor.MoveCaretDown(rand.Next() % 7 * (rand.Next() % 2 == 1 ? -1 : 1));
+						break;
+				}
+			}
+		}
+	}
 }
